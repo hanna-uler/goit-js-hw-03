cd295119b495e4d3da2fff51fc47d5ebf80bc948
hanna-uler <hannauler@gmail.com>
2025-01-14 14:24:11 -0500

    complete task-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,9 @@
-function makeTransaction(quantity, pricePerDroid) {
-    const totalPrice = quantity * pricePerDroid;
-    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-};
-console.log(makeTransaction(5, 3000));
-console.log(makeTransaction(3, 1000));
-console.log(makeTransaction(10, 500));
+function slugify(title) {
+    const lowerCaseTitle = title.toLowerCase();
+    const titleWords = lowerCaseTitle.split(" ");
+    return titleWords.join("-");
+}
+console.log(slugify("Arrays for beginners")); // "arrays-for-beginners"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
